33966efbc7b246b2a24ac616a70d36a930547194
Ramana Nagasamudram <ramana.nagasam@gmail.com>
2016-10-26 23:56:48 +0530

    Delete callGeometry.m

--- callGeometry.m
+++
@@ -1,9 +0,0 @@
-function [a3,delta] = callGeometry(platSize)
-%Function to calculate certain geometric values of the manipulator
-
-
-a3 = sqrt((0.5*platSize(1))^2+(0.5*platSize(2))^2);
-delta = [atan2(platSize(2),platSize(1)),atan2(platSize(2),-platSize(1)),...
-    atan2(-platSize(2),-platSize(1)),atan2(-platSize(2),platSize(1))];
-
-end
